775e05ac785140128d641172fc2a7c12576b09af
Kempis Guerrero <kempisguerrero96@gmail.com>
2021-07-28 18:31:15 -0500

    Se agregaron middlewares
    Morgan
    Express.json
    También se agregó en routes el archivo task.routes.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-//settings
+//Settings
 app.set('port', process.env.PORT || 3000);
 
+//Middlewares
+app.use(morgan('dev')); //sirve para tener información sobre cuanto tiempo tardo para la petición, código de error, etc
+app.use(express.json()); //comprueba si la información que llega es un json y así mismo se podrán enviar archivos con formato json
+
+//Routes
+
+//Static files
+
+//Starting the server
 app.listen(app.get('port'), () => {
   console.log(`Server on port ${app.get('port')}`);
 });
