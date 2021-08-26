fc8099a36e0ff387220e371293024077fe68523c
Kempis Guerrero <kempisguerrero96@gmail.com>
2021-08-26 18:10:54 -0500

    Rutas de usuario
    Login y registro

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,65 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { Schema } = mongoose;
+const saltRounds = 10;
 
 const UserSchema = new Schema({
   nombre: { type: String, required: true },
   correo: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
 });
 
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+UserSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+UserSchema.methods.generateToken = function (cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), 'secret');
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+UserSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  jwt.verify(token, 'secret', function (err, decode) {
+    user.findOne({ _id: decode, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
 module.exports = mongoose.model('User', UserSchema);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -16,7 +16,27 @@ router.get('/:id', async (req, res) => {
 
 //POST-LOGIN
 router.post('/login', async (req, res) => {
-  User.findOne({ correo: req.body.correo });
+  await User.findOne({ correo: req.body.correo }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSucces: false,
+        message: 'Auth failed, email not found',
+      });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: 'Wrong password' });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie('w_authExp', user.tokenExp);
+        res.cookie('w_auth', user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
+        });
+      });
+    });
+  });
 });
 
 //POST-REGISTRO
