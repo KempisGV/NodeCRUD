27941c7a56a72683560d8ece24669b8fbea6b910
mathiasloor <mathiasloor@uees.edu.ec>
2021-09-21 18:07:52 -0500

    Preparación para deploy
    Preparación para deploy

--- server/database.js
+++ server/database.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
-const password = 'KGkerRI84lxMs3uy';
-const database = 'nodecrud';
 
-const URI = `mongodb+srv://Kempis:${password}@cluster0.q3zod.mongodb.net/${database}?retryWrites=true&w=majority`;
 
 mongoose
-  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(db => console.log('DB is connected'))
   .catch(err => console.log(err));
 
